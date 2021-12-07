c0457749bc56fbf36d07756a16ab332877b69339
Jake Honschopp <Jakehonschopp@yahoo.com>
2021-12-07 18:17:31 -0500

    committing hopeful final code

--- baq.js
+++ baq.js
@@ -1,1 +1,22 @@
-js for quiz
+
+
+function result() {
+	let score = 0; 
+	if (document.getElementById("correct1").checked)
+	{ 
+		score++;
+	} 
+	if (document.getElementById("correct2").checked)
+	{
+		score++;
+	}
+	if (document.getElementById("correct3").checked)
+	{
+		score++;
+	}
+	if (document.getElementById("correct4").checked)
+	{
+		score++; 
+	}
+	document.write("Correct Answers: 1 - Steven Spielberg 2 - 'Rosebud' 3 - Shirley Henderson (Moaning Myrtle) 4 - Joker. Your score is:"+score); 
+} 
